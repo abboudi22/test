f30d55d5bf0212874321c67c6b3b0d041f487932
abboud abboud <aboude.aboude221@gmail.com>
2018-05-23 02:46:46 +0300

    add2

--- index.php
+++ index.php
@@ -1,27 +1,10 @@
-<?php
-    require_once('functions.inc');
-    require_once('database.php');
-    $f = scandir(__DIR__);
-    linking($f);
+<?php 
+//$h = fopen('test.txt','r');
+//$val = fread($h,filesize('test.txt'));
+//echo $val;
+//fclose($h);
+//$h1 = fopen('test.txt','w');
+//$val++;
+//fwrite($h1,$val);
+//fclose($h1);
 ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>AJAx</title>
-    </head>
-    <body>
-    <div class="main">
-    <h1>todo app</h1>
-    <form name='forme'>
-      name:<input type='text' name='name' />
-      age:<input type='text' name='age'/>
-    </form>
-    <button onclick='enableAjax(this.value,"box",)' value='add'>ADD</button>
-    <div class="container" >
-
-    <div id="box"><?php getdata();?></div>
-    </div>
-</div>
-  </body>
-</html>
